02ce7a8f394d6834bd2245a318a4b286fa74529f
junkkerrigan <726721gfd>
2018-02-10 10:12:31 +0200

    began workflow page

---
+++ src/components/workflow-page/Task.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import '../../scss/workflow-page/Task.scss';
+
+class Task extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <li className='task-item'>
+
+            </li>
+        );
+    }
+}
+
+export default Task;
